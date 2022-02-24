77843f37b0c658b87f82f36f0353e35e12fed8c8
deeppak977 <88644177+deeppak977@users.noreply.github.com>
2022-02-24 12:46:30 +0530

    messege

---
+++ source/repos/pppp/pppp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Prop
+{
+    class Prop_EX
+    {
+        int data;
+        public int kata
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+    }
+    class MAIN
+    {
+        public static void Main(string[]args)
+        {
+            Prop_EX obj = new ();
+            obj.kata=10;
+            Console.WriteLine(obj.kata);
+          //  Console.WriteLine(obj.data);
+            Console.ReadKey();
+
+        }
+    }
+}
